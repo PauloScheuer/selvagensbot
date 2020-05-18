6ac8eb2fef132eb6e6b188917c593e63b5bf8022
PauloScheuer <62629566+PauloScheuer@users.noreply.github.com>
2020-05-17 21:32:10 -0300

    First commit

---
+++ data.js
@@ -0,0 +1,49 @@
+var shuffle = require('shuffle-array');
+var fs = require('fs');
+var selvagens = JSON.parse(fs.readFileSync('./selvagens.json', 'utf-8'));
+
+var posts = JSON.parse(fs.readFileSync('./posts.json', 'utf-8'));
+
+//escreve json com todas as frases, com base no selvagens.json;
+function getPhrases() {
+  var i = 0;
+  var numAlb = selvagens.length;
+  while (i < numAlb) {
+    //acessa os álbuns
+    var j = 0;
+    var albumObj = selvagens[i];
+    var numSong = albumObj['songs'].length;
+    while (j < numSong) {
+      //acessa as músicas
+      var songObj = albumObj['songs'][j];
+      var nameSong = Object.keys(songObj)[0];
+      var numPhrase = songObj[nameSong].length;
+      var k = 0;
+      while (k < numPhrase) {
+        //acessa as frases
+        var content = {};
+        var phrase = songObj[nameSong][k];
+        content['albumName'] = albumObj['name'];
+        content['albumYear'] = albumObj['year'];
+        content['nameSong'] = nameSong;
+        content['phrase'] = phrase;
+        var indexInToCome = posts['toCome'].findIndex(
+          (val) => val.phrase == phrase
+        );
+        var indexInAlready = posts['already'].findIndex(
+          (val) => val.phrase == phrase
+        );
+        if (indexInToCome < 0 && indexInAlready < 0) {
+          posts['toCome'].push(content);
+        }
+
+        k++;
+      }
+      j++;
+    }
+    i++;
+  }
+}
+getPhrases();
+//escreve
+fs.writeFileSync('./posts.json', JSON.stringify(posts));
---
+++ index.js
@@ -0,0 +1,53 @@
+var Twit = require('twit');
+require('dotenv').config();
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var schedule = require('node-schedule');
+var posts = JSON.parse(fs.readFileSync('posts.json', 'utf-8'));
+
+//adiciona ao array.prototype uma função que retorna um número aleátorio
+Array.prototype.randomElement = function () {
+  return this[Math.floor(Math.random() * this.length)];
+};
+
+const Bot = new Twit({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  timeout_ms: 60 * 1000,
+});
+
+//define a porta do app
+var porta = process.env.PORT || 8080;
+app.listen(porta);
+
+//função do bot
+var j = schedule.scheduleJob({ hour: 15, minute: 0 },
+  function botInit() {
+    var postNow = posts['toCome'].randomElement();
+    var indexNow = posts['toCome'].indexOf(postNow);
+    posts['toCome'].splice(indexNow, 1);
+    posts['already'].push(postNow);
+    if (posts['toCome'].length > 0) {//se ainda tiver posts
+      var status = postNow['phrase'] + "\n - " + postNow['nameSong'] +
+        " (" + postNow['albumName'] + ", " + postNow['albumYear'] + ")";
+      Bot.post('statuses/update', {
+        status: status
+      },
+        function (err, data, response) {
+          fs.writeFileSync('./posts.json', JSON.stringify(posts));
+        });
+    } else {//quando não tiver mais posts
+      Bot.post('statuses/update', {
+        status: "Fim do Bot! Obrigado por acompanhar"
+      },
+        function (err, data, response) {
+          j.cancel();
+        });
+    }
+  }
+);
+//time = setInterval(botInit, 60 * 1000);
+//botInit();
